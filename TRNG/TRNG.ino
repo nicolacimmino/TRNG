e6d91f4a6a580c2ffe0a340c943552d9ef65c0dc
Nicola Cimmino <nicola.cimmino@gmail.com>
2017-12-16 12:20:18 +0000

    Apply code formtting

--- TRNG/TRNG.ino
+++ TRNG/TRNG.ino
@@ -15,7 +15,6 @@
 //    along with this program.  If not, see http://www.gnu.org/licenses/.
 //
 
-
 #define INPUT_SIGNAL_PIN A0
 
 #define OUTPUT_BLOCK_SIZE 1024
@@ -24,62 +23,60 @@
 #define PH1 12
 
 // Total amount of random bytes oputput so far.
-uint32_t outputBytes=0;
+uint32_t outputBytes = 0;
 
 void setup()
 {
   Serial.begin(112500);
-  
+
   pinMode(INPUT_SIGNAL_PIN, INPUT);
-  
-   // Set ADC prescaler to 16 so we get higher sample 
-   // rate than with default settings.
-  _SFR_BYTE(ADCSRA) |=  _BV(ADPS2); // Set ADPS2
+
+  // Set ADC prescaler to 16 so we get higher sample
+  // rate than with default settings.
+  _SFR_BYTE(ADCSRA) |= _BV(ADPS2);  // Set ADPS2
   _SFR_BYTE(ADCSRA) &= ~_BV(ADPS1); // Clear ADPS1
   _SFR_BYTE(ADCSRA) &= ~_BV(ADPS0); // Clear ADPS0
-  
+
   // Use internal 1.1V reference
   analogReference(INTERNAL);
 
   pinMode(PH0, OUTPUT);
   pinMode(PH1, OUTPUT);
-  
 }
 
 void loop()
 {
-  
-  // Temporay storage for output random values.
-  byte static randomOut=0;
- 
-  // Tempoary storage for output from the whitening process.
-  byte static whitenedOut=0;
-  
-  // Amount of bits produced by the whitening pocess.
-  byte static whitenedBitsCount=0;
-  
-  // Current input signal average value.
-  float static average=0;
 
-  // Collect 8 random bits by keeping track of the 
+  // Temporay storage for output random values.
+  byte static randomOut = 0;
+
+  // Tempoary storage for output from the whitening process.
+  byte static whitenedOut = 0;
+
+  // Amount of bits produced by the whitening pocess.
+  byte static whitenedBitsCount = 0;
+
+  // Current input signal average value.
+  float static average = 0;
+
+  // Collect 8 random bits by keeping track of the
   // average signal level with a long IIR low pass
   // and sampling a 1 bit when the current signal is
   // above the average and 0 otherwise.
-  
-  for(int ix=0; ix<8;ix++)
+  for (int ix = 0; ix < 8; ix++)
   {
     // Keep the charge pump running so that
     // C3 stays above 12v and the zener is
     // in breakdown.
-    digitalWrite(PH0, (ix%2==0)?HIGH:LOW);
-    digitalWrite(PH1, (ix%2==0)?LOW:HIGH);
+    digitalWrite(PH0, (ix % 2 == 0) ? HIGH : LOW);
+    digitalWrite(PH1, (ix % 2 == 0) ? LOW : HIGH);
     delay(1);
 
     int noiseReading = analogRead(INPUT_SIGNAL_PIN);
-    average=0.99f*average+0.01f*noiseReading;
-    randomOut = (randomOut<<1)|((noiseReading>average)?1:0);
-  }  
-  
+    average = 0.99f * average + 0.01f * noiseReading;
+    randomOut = (randomOut << 1) | ((noiseReading > average) ? 1 : 0);
+  }
+
   // The data collected so far might be biased, we do some
   // whitening applying John von Neumann whitening algoirthm.
   // The algorithm consumes 2+ bits to generate one bit, the
@@ -88,28 +85,28 @@ void loop()
   // if they are equal they are discarded if they are "10" then
   // a 1 is taken as output, if they are "01" then 0 is taken
   // as output.
-  for(int ix=0;ix<8;ix+=2)
-  {    
-    if((randomOut&1) != ((randomOut>>1)&1))
+  for (int ix = 0; ix < 8; ix += 2)
+  {
+    if ((randomOut & 1) != ((randomOut >> 1) & 1))
     {
-      whitenedOut=(whitenedOut<<1)|(randomOut&1);
+      whitenedOut = (whitenedOut << 1) | (randomOut & 1);
       whitenedBitsCount++;
-      if(whitenedBitsCount==8)
+      if (whitenedBitsCount == 8)
       {
         outputDataHex(whitenedOut);
-        whitenedBitsCount=0;
+        whitenedBitsCount = 0;
       }
-     }
-     randomOut=randomOut>>2;
-   }
-   
-   if(outputBytes>=OUTPUT_BLOCK_SIZE)
+    }
+    randomOut = randomOut >> 2;
+  }
+
+  /*if(outputBytes>=OUTPUT_BLOCK_SIZE)
    {
      while(Serial.read()>0);
      while(!Serial.available());
      Serial.println("");
      outputBytes=0;
-   }
+   }*/
 }
 
 /*
@@ -118,8 +115,8 @@ void loop()
  */
 void outputDataHex(byte randomNumber)
 {
-  Serial.print("0123456789ABCDEF"[randomNumber>>4]);
-  Serial.print("0123456789ABCDEF"[randomNumber&0xF]);
-  Serial.print((outputBytes%32==31)?"\r\n":""); 
-  outputBytes++;  
+  Serial.print("0123456789ABCDEF"[randomNumber >> 4]);
+  Serial.print("0123456789ABCDEF"[randomNumber & 0xF]);
+  Serial.print((outputBytes % 32 == 31) ? "\r\n" : "");
+  outputBytes++;
 }
