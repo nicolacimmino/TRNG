840bd0a52d494f9a9cf397466ed106272a6d173e
Nicola Cimmino <nicola.cimmino@gmail.com>
2018-05-03 22:00:33 +0000

    Add speed

--- validation/collect.sh
+++ validation/collect.sh
@@ -14,6 +14,8 @@ WANTED_BLOCKS=`expr $WANTED_BYTES / 32`
 
 mkdir -p data
 
+START=`date +%s`
+
 stty -F /dev/ttyUSB0 115200
 cat /dev/ttyUSB0 \
 	 | sed -u -e "s/^\.*//g" \
@@ -21,3 +23,7 @@ cat /dev/ttyUSB0 \
 	 | dd  iflag=fullblock bs=96 count=$WANTED_BLOCKS status=none \
 	 | tee data/$1.hex
 
+STOP=`date +%s`
+SPEED="$(( $WANTED_BYTES * 8 / ( $STOP - $START ) ))"
+
+echo "$SPEED bit/s"
